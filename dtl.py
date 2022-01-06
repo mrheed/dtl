cb4b8f23374430e42668d18256ec1d98b4d0c930
syahidnurrohim <syahidnurrohim@gmail.com>
2022-01-06 23:16:07 +0700

    update dari lama

--- dtl.py
+++ dtl.py
@@ -6,7 +6,9 @@ def sample_data():
   merek = ["M" + str(i) for i in range(1, 15)]
   layak = (['Y'] * 3) + ['T'] + (['Y'] * 4) + ['T'] + ['Y'] + (['T'] * 4)
   baterai = (['Kuat'] * 4) + (['Cukup'] * 5 + (['Lemah'] * 5))
-  return np.c_[hp, baterai, layak, merek]
+  kamera = (['Tinggi'] * 2) + ['Sedang'] + ['Rendah'] + ['Tinggi'] + (['Sedang'] * 2) + ['Tinggi'] + ['Rendah'] + (['Tinggi'] * 2) + (['Sedang'] * 2) + ['Rendah']
+  harga = (['Sangat Murah'] * 2) + (['Mahal'] * 2) + ['Sangat Murah'] + ['Mahal'] + ['Sangat Murah'] + (['Murah'] * 2) + (['Sangat Murah'] * 2) + (['Mahal'] * 2) + ['Sangat Mahal']
+  return np.c_[hp, baterai, kamera, merek, harga, layak]
 
 def index_in_list(l,i):
   return len(l) < i
@@ -35,25 +37,23 @@ return:
 """
 def groupFn(data, pivot = None, ret_col_idx = None):
   def create_key(d):
-    d = [d] if not isinstance(d, list) else d 
-    d.sort()
+    if not isinstance(d, list):
+      if isinstance(d, np.ndarray):
+        d = d.tolist()
+      else:
+        d = [d]
     return ".".join(d)
   if (pivot is None):
     pivot = data
-  distinct = np.unique(pivot)
   mapped = {}
-  grouped = [None] * len(distinct)
-  for i, dist in enumerate(distinct):
-    key = create_key(dist)
-    mapped[key] = i
   for i, d in enumerate(data):
     key = create_key(pivot[i])
-    real_index = mapped[key]
-    if grouped[real_index] is None:
-      grouped[real_index] = []
     d = d[ret_col_idx] if ret_col_idx is not None else d
-    grouped[real_index].append(d)
-  return mapped.keys(), grouped
+    if key not in mapped:
+      mapped[key] = [d]
+    else:
+      mapped[key].append(d)
+  return mapped.keys(), mapped.values()
 
 """
 Hitung nilai entropy (heterogenitas) data
@@ -82,10 +82,16 @@ def split_information(sample, attribute):
   headers, grouped = groupFn(cctd, cctd[:,1], ret_col_idx=0)
   return sum(-float(len(grp)/total)*np.log2(float(len(grp)/total)) for grp in grouped)
 
+def id3(sample, target_attribute, attributes):
+  pass
+
 def main():
   sample = sample_data()
-  gain_ratio_val = gain_ratio(np.array(sample[:, 2]), np.array(sample[:, 1]))
-  print(gain_ratio_val)
+  keys, grouped = groupFn(sample, sample[:,1:3], ret_col_idx=0)
+  print(entropy(sample[:,2]))
+  #gain_ratio_val = gain_ratio(np.array(sample[:, len(sample[0])-1]), np.array(sample[:, 2]))
+  #print(tabulate(sample))
+  #print(gain_ratio_val)
 
 if __name__ == '__main__':
   main()
